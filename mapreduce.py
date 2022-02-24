a0f86fc3efe6b693906a25a119cd56b2e6555aa0
chinolla <cjchinolla@miners.utep.edu>
2022-02-23 21:51:10 -0700

    added comments, changed the pymp.parrallel value to thread instead of just 1

--- mapreduce.py
+++ mapreduce.py
@@ -1,16 +1,17 @@
+# Cristobal Chinolla
+# Lab 2 Map Reduce
+
+
+
 import re
 import pymp
 import time
 
-listofFiles = ('shakespeare1.txt', 'shakespeare2.txt', 'shakespeare3.txt',
-        'shakespeare4.txt', 'shakespeare5.txt', 'shakespeare6.txt',
-        'shakespeare7.txt' ,'shakespeare8.txt')
-
-listofWords = ('hate', 'love', 'death', 'night', 'sleep', 'time', 'henry', 'hamlet',
-        'you', 'my', 'blood', 'poison', 'macbeth', 'king', 'heart', 'honest')
+listofFiles = ('shakespeare1.txt', 'shakespeare2.txt', 'shakespeare3.txt', 'shakespeare4.txt', 'shakespeare5.txt', 'shakespeare6.txt', 'shakespeare7.txt' ,'shakespeare8.txt')
+listofWords = ('hate', 'love', 'death', 'night', 'sleep', 'time', 'henry', 'hamlet', 'you', 'my', 'blood', 'poison', 'macbeth', 'king', 'heart', 'honest')
 
 
-
+# function returns the count (int) of occurences of a word in a file
 def countWords(word, filename):
     count = 0
     with open(filename, 'r') as file:
@@ -25,20 +26,21 @@ def mapreduce(thread):
     lock = p.lock
     maindict = pymp.shared.dict()
     
-
+    # the main dictionary that the function will return, sets all values to zero for all words
     for word in listofWords:
             maindict[word] = 0
 
-    with pymp.Parallel(1) as p:
-    
+    with pymp.Parallel(thread) as p:
+        
+        # sets the local dictionary used by threads, all values to 0
         threaddict = dict()
         for word in listofWords:
             threaddict[word] = 0
-
+        # updates the local dictionary with word count occurences for each word and each file
         for file in p.range(len(listofFiles)):
             for word in listofWords:
                 threaddict[word] += countWords(word, listofFiles[file])
-        
+        # updating the main dictionary, locking before edits and releasing after finished
         for word in listofWords:
             lock.acquire()
             maindict[word] += threaddict[word]
